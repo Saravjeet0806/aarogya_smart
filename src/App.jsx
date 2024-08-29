3c0a1a2d46f44c20f77810bdbed7a7f04b7d0554
Sarav <Saravjeetofficial@gmail.com>
2024-08-29 07:55:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-200">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
