9a8151a07f98d2ea3713f0f6b7ebd12e6182f193
Sarav <Saravjeetofficial@gmail.com>
2024-08-31 17:00:47 +0530

    added bmi calculator on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import backgroundImage from '../assets/images/yogaman.jpg';
+import BMICalculator from './BMICalculator';
 
 const HomePage = () => {
   return (
-    <section
-   id="home"
-       className="relative bg-cover bg-center h-screen"
-    >
-      <div className="absolute "></div>
-      <div className="container mx-auto px-4 py-20 text-center relative z-10 flex flex-col justify-center h-full">
-        <h1 className="text-4xl font-bold text-white mb-auto">Achieve Your Fitness Goals</h1>
-        <div className="absolute bottom-20 right-10">
+    <section className="relative min-h-screen flex items-center justify-center">
+      <div className="container mx-auto px-4 py-20 text-center relative z-10">
+        <h1 className="text-4xl font-bold text-white mb-6 ">Achieve Your Fitness Goals</h1>
+        <div className="flex flex-col items-center mb-auto">
+          <BMICalculator />
+        </div>
+        <div className="absolute bottom-10 right-10">
           <a href="#workout-plan" className="px-8 py-3 bg-red-600 text-white rounded-full hover:bg-red-700">
             Your Plan
           </a>
