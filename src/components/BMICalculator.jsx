eefe1e3e2d56552869010ec39400cf17ef1968c0
Sarav <Saravjeetofficial@gmail.com>
2024-08-31 17:00:29 +0530

    fixed button color

--- src/components/BMICalculator.jsx
+++ src/components/BMICalculator.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState } from 'react';
 
 const BMICalculator = () => {
@@ -16,12 +15,12 @@ const BMICalculator = () => {
   };
 
   return (
-    <section className="container mx-auto px-4 py-16">
+    <section className="container mx-auto px-4 py-10">
       <h2 className="text-2xl font-bold text-center mb-8">Calculate Your BMI</h2>
-      <div className="max-w-md mx-auto bg-white p-8 shadow-md rounded-lg">
+      <div className="max-w-md mx-auto p-2 ">
         <form onSubmit={calculateBMI}>
           <div className="mb-4">
-            <label className="block text-gray-700">Height (cm)</label>
+            <label className="block text-white">Height (cm)</label>
             <input
               type="number"
               className="mt-1 p-2 w-full border rounded"
@@ -40,7 +39,7 @@ const BMICalculator = () => {
               onChange={(e) => setWeight(e.target.value)}
             />
           </div>
-          <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">Calculate</button>
+          <button className="w-full bg-red-600 text-white py-2 rounded hover:bg-blue-700">Calculate</button>
         </form>
         {bmi && (
           <div className="mt-4 text-center">
