3aa7535ba1ec4d2394758aa63c37693deaed08dc
Sarav <Saravjeetofficial@gmail.com>
2024-08-31 10:57:08 +0530

    re-arranged folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import BMICalculator from './components/BMICalculator';
 
 import './App.css'
 
@@ -7,9 +10,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-200">
-      Hello world!
-    </h1>
+      <Navbar />
+      <HomePage />
+      <BMICalculator />
     </>
   )
 }
