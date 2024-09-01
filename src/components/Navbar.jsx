322df25b9ee21f123680fa39cba9c15d585cfab9
Sarav <Saravjeetofficial@gmail.com>
2024-09-01 18:46:34 +0530

    fixed logo and font color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
     <nav className="">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="text-3xl font-bold">YourLogo</div>
+        {/* Logo */}
+        <div className="flex items-center">
+          
+          <div className="text-3xl font-bold">AarogyaAi</div>
+        </div>
         <ul className="flex space-x-6">
-          <li><Link to="/" className="text-white text-lg hover:text-gray-900">Home</Link></li>
-          <li><Link to="/daily-challenge" className="text-white text-lg hover:text-gray-900">Daily Challenge</Link></li>
-          <li><Link to="/workout-plan" className="text-white text-lg hover:text-gray-900">Workout Plan</Link></li>
-          <li><Link to="/motivation" className="text-white text-lg hover:text-gray-900">Motivation</Link></li>
-          <li><Link to="/community" className="text-white text-lg hover:text-gray-900">Community</Link></li>
-          <li><Link to="/profile" className="text-white text-lg hover:text-gray-900">Profile</Link></li>
+          <li><Link to="/" className="text-[#e7e6a7] text-lg hover:text-gray-900">Home</Link></li>
+          <li><Link to="/daily-challenge" className="text-[#e7e6a7] text-lg hover:text-gray-900">Daily Challenge</Link></li>
+          <li><Link to="/workout-plan" className="text-[#e7e6a7] text-lg hover:text-gray-900">Workout Plan</Link></li>
+          <li><Link to="/motivation" className="text-[#e7e6a7] text-lg hover:text-gray-900">Motivation</Link></li>
+          <li><Link to="/community" className="text-[#e7e6a7] text-lg hover:text-gray-900">Community</Link></li>
+          <li><Link to="/profile" className="text-[#e7e6a7] text-lg hover:text-gray-900">Profile</Link></li>
         </ul>
       </div>
     </nav>
@@ -20,3 +25,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
