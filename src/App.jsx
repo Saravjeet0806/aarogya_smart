9c72f9585182ab6a00abe27e0e0721c12f70878d
Sarav <Saravjeetofficial@gmail.com>
2024-09-02 08:21:19 +0530

    changes in app.jsx to include sign in and sign up logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,19 @@ import WorkoutPlan from './components/WorkoutPlan';
 import Motivation from './components/Motivation';
 import Community from './components/Community';
 import Profile from './components/Profile';
-import Img from './assets/images/yogaman2.jpg'; 
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase'; 
+import Img from './assets/images/yogaman2.jpg'
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div
       className="relative min-h-screen"
@@ -34,7 +44,16 @@ function App() {
               <Route path="/workout-plan" element={<WorkoutPlan />} />
               <Route path="/motivation" element={<Motivation />} />
               <Route path="/community" element={<Community />} />
-              <Route path="/profile" element={<Profile />} />
+              
+              {/* Protected Routes */}
+              <Route 
+                path="/profile" 
+                element={user ? <Profile /> : <Login />} 
+              />
+              
+              {/* Public Routes */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
             </Routes>
           </div>
         </div>
